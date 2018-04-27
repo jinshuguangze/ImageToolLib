7f0bedd3a3e09160cfb8c9de820cc1c8fb492d28
jinshuguangze <jinshuaguangze@gmail.com>
2018-04-27 19:30:51 +0800

    增加注释，修正BUG
    好了，接下来的问题就是如何结合边界函数了

--- regionExpanding_Gray.m
+++ regionExpanding_Gray.m
@@ -1,10 +1,13 @@
 function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
-%regionExpanding:使用区域膨胀法将灰度图像分割单例化
-%inputImage:输入图像，可以为灰度图像或者二值图像或者RGB
-%filter:给定初始灰度阈值
-%degree:给定新像素允许灰度波动最大值
+%regionExpanding_Gray:使用区域膨胀法将灰度图像分割单例化
+%inputImage:输入图像，指定为灰度图像，且感兴趣区域为深色区域
+%degree:新像素允许灰度波动的范围，允许范围是0~0.5
+%outputNum:希望输出的至多图像个数，如果不输出或输入0，则会基于像素点个数下降梯度智能选择图像个数
+%estimated:感兴趣区域的灰度估计值，如果不输入，则会基于Otsu算法自动得到统计意义上的估计值
+%method:识别边缘的方法，能使用‘Sobel’，‘Prewitt’，‘Roberts’，‘Log’，‘Zerocross’，’Canny‘，’Approxcanny‘这七种方法
+%operator:二维膨胀聚合算子，能使用’Low‘，’Medium‘，’High‘，’Extra‘四种等级来使用对应的内建算子
 %outputImages:输出图像细胞数组，每个元胞都是一个单例图像
-%versin:1.0.5
+%versin:1.0.6
 %author:jinshuguangze
 %data:4/13/2018
 %State0:未扫描的像素点
@@ -14,29 +17,29 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
 %TODO:写个自动分配内存器(√)，支持RGB等，取消filter参数改为自动判断
        
     %入口检测
-    p=inputParse;%构造检测器对象
+    p=inputParser;%构造检测器对象
     %输入图像，限定为灰度图像，且需要分割的图像为深色，支持多种灰度等级的灰度图像
     p.addRequired('inputImage',@(x)validateattributes(x,{'numeric'},...
-        {'2d','integer','positive'},'regionExpanding_Gray'),'inputImage',1);
+        {'2d','integer','nonnegative'},'regionExpanding_Gray','inputImage',1));
     %灰度允许范围值，限定为0到0.5之间的数，数字越小，运算越快
     p.addRequired('degree',@(x)validateattributes(x,{'double'},...
-        {'sclar','>',0,'<',0.5},'regionExpanding_Gray'),'degree',2);
+        {'scalar','>',0,'<',0.5},'regionExpanding_Gray','degree',2));
     %输出图像的个数，如果不输入或者输入0，则默认输出包含像素个数梯度最大的点之前的所有图像
-    p.addOptional('number',0,@(x)validateattributes(x,{'numeric'},...
-        {'sclar','integer','positive'},'regionExpanding_Gray'),'number',3);   
+    p.addOptional('outputNum',0,@(x)validateattributes(x,{'numeric'},...
+        {'scalar','integer','nonnegative'},'regionExpanding_Gray','outputNum',3));   
     %灰度估计值，如果不输入，则使用Otsu算法获得得灰度减去输入的灰度允许范围值
     p.addOptional('estimated','None',@(x)validateattributes(x,{'double'},...
-        {'sclar','>=',0,'<=',1},'regionExpanding_Gray'),'estimated',4);   
+        {'scalar','>=',0,'<=',1},'regionExpanding_Gray','estimated',4));   
     %识别边缘的方法，支持所有在库函数’edge‘中出现的方法，默认为‘Canny’方法
     p.addParameter('method','Robert',@(x)any(validatestring(x,...
-        {'Sobel','Prewitt','Roberts','Log','Zerocross','Canny','Approxcanny'})));
+        {'Sobel','Prewitt','Roberts','Log','Zerocross','Canny','Approxcanny'},'regionExpanding_Gray','method',5)));
     %二维聚合算子，支持四种从小到大的范围，范围越小，运算越快，默认为’Low‘算子的四联通区域
     p.addParameter('operator','Low',@(x)any(validatestring(x,...
-        {'Low','Medium','High','Extra'})));
+        {'Low','Medium','High','Extra'},'regionExpanding_Gray','operator',6)));
     p.parse(inputImage,degree,varargin{:});%检测  
-    inputImage=p.Results.inputImgae;%赋值
+    inputImage=p.Results.inputImage;%赋值
     degree=p.Results.degree;
-    number=p.Results.number;
+    outputNum=p.Results.outputNum;
     estimated=p.Results.estimated;
     method=p.Results.method;
     operator=p.Results.operator;
@@ -47,6 +50,7 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
     stateImage=zeros(row,col);%初始化状态表
     edgeImage=edge(inputImage,method);%初始化边缘表
     count=0;%初始化输出图像计数器
+    gather={};%初始化存储所有输出图像的聚集数组
     
     if estimated=='None'%如果估计值没有输入
         thresh=graythresh(inputImage);%获取统计意义上的最优阈值
@@ -57,19 +61,22 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
         end
     end
     
-    switch operator%二维聚合算子实例化
-        case 'Low'
+    switch upper(operator)%二维聚合算子实例化
+        case 'LOW'
             neibor=[-1 0;0 1;1 0;0 -1];
             
-        case 'Medium'
+        case 'MEDIUM'
             neibor=[-1 0;0 1;1 0;0 -1;-1 -1;-1 1;1 1;1 -1];
             
-        case 'High'
+        case 'HIGH'
             neibor=[-1 0;0 1;1 0;0 -1;-1 -1;-1 1;1 1;1 -1;0 -2;-2 0;0 2;2 0];
             
-        case 'Extra'
+        case 'EXTRA'
             neibor=[-1 0;0 1;1 0;0 -1;-1 -1;-1 1;1 1;1 -1;0 -2;-2 0;0 2;2 0;
                 2 -1;2 -2;1 -2;-1 -2;-2 -2;-2 -1;-2 1;-2 2;-1 2;1 2;2 2;2 1];
+            
+        otherwise%由于validatestring的特性，会接受一些奇怪的近似字符进来，此时只能设定为默认值
+            neibor=[-1 0;0 1;1 0;0 -1];
     end
     
     %找出满足条件的生长开始点
@@ -82,7 +89,6 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
                     %初始化预设值    
                     handleList=[i,j,inputImage(i,j)];%初始化待邻域检测列表
                     fulfilList=[];%初始化完成邻域检测的列表
-                    gather=cell;%初始化存储所有输出图像的聚集数组
                     adv=inputImage(i,j);%初始化平均值
                     top=i;%初始化图像范围值
                     bottom=i;
@@ -144,20 +150,29 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
         outputImages{1}=ones(row,col);
     elseif count==1%只有一副图像，直接输出
         outputImages=gather{1,1};
-    else%有多幅图像，用冒泡排序由大到小排下来
+    else%有多幅图像，用冒泡排序降序排列
         for m=1:count
             for n=2:count
                 if gather{n-1,2}<gather{n,2}
-                    gather{[n-1,n],:}=gather{[n,n-1],:};%交换两行
+                    tempA=gather{n-1,1};%交换两行
+                    tempB=gather{n-1,2};
+                    gather{n-1,1}=gather{n,1};
+                    gather{n-1,2}=gather{n,2};
+                    gather{n,1}=tempA;
+                    gather{n,2}=tempB;
                 end
             end
         end
         
-        if number%如果规定了输出图像数目
-            if count>number%输出指定数量图像
-                outputImages=gather{1:number,1};
+        if outputNum%如果规定了输出图像数目
+            if count>outputNum%输出指定数量图像
+                for o=1:outputNum
+                    outputImages{o}=gather{o,1};
+                end
             else%输出所有图像
-                outputImages=gather{:,1};
+                for o=1:count
+                    outputImages{o}=gather{o,1};
+                end
             end
         else%如果没有规定输出图像数目，则选择梯度下降最陡的点之前的图像
             maxGrad=0;%初始化最大梯度
@@ -167,7 +182,9 @@ function outputImages = regionExpanding_Gray(inputImage,degree,varargin)
                     indexMax=v-1;%记录此序号
                 end
             end
-            outputImages=gather{1:indexMax,1};              
+            for o=1:indexMax
+                outputImages{o}=gather{o,1};
+            end
         end
     end
 end
